9cb9fb4dec6263c523c3b92372446869dd71b3a1
HACKWITHNESBITT <ronov610@gmail.com>
2026-08-21 20:29:46 +0300

    Fix Render frontend routing

--- backend/actions/home.go
+++ backend/actions/home.go
@@ -2,12 +2,17 @@ package actions
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/envy"
 )
 
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
+	if origin := strings.TrimRight(envy.Get("FRONTEND_ORIGIN", ""), "/"); ENV == "production" && origin != "" {
+		return c.Redirect(http.StatusTemporaryRedirect, origin)
+	}
 	return c.Render(http.StatusOK, r.HTML("home/index.plush.html"))
 }
